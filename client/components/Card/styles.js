503d557774f11d1e69c1372a6e9c2c99bf726bbd
bandeyaatif <aatif.bandey@tokopedia.com>
2020-12-31 23:09:05 +0530

    Added media query

--- client/components/Card/styles.js
+++ client/components/Card/styles.js
@@ -5,26 +5,32 @@ export const singleCard = css`
 	display: flex;
 	width: 30%;
 	border: 1px solid #ccc;
-  margin: 1% 0% 1% 1%;
-  border-radius: 4px;
+  	margin: 1% 0% 1% 1%;
+  	border-radius: 2px;
 	border-left: 0px;
 	border-top: 0px;
 	border-right: 0px;
 	padding: 10px;
+	font-size: 13px;
+	flex-direction: column;
 	img {
 		width: 100%;
 	}
+	@media (min-width: 768px) {
+		width: 28%;
+		font-size: 16px;
+    }
 `;
 
 export const content = css`
 	display: flex;
 	flex-direction: column;
 	justify-content: start;
-	margin: 0 0 0 2%;
-	width: 55%;
+	
+	width: 100%;
 	div {
 		word-break: break-all;
-		margin: 8% 0 0 2%;
+		margin: 2% 0 0 0%;
 	}
 `;
 export const imgHolder = css`

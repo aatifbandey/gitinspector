94f92b1122bf13a1612afe7ccd098ee0eb0f9a81
bandeyaatif <aatif.bandey@tokopedia.com>
2021-01-01 15:24:15 +0530

    Identation fix

--- webpack.config.js
+++ webpack.config.js
@@ -25,23 +25,17 @@ module.exports = {
                 "test": /\.(js|jsx)$/,
                 "exclude": /node_modules/,
                 "use": {
-										"loader": "babel-loader",
-										options: {
-												presets: ['@babel/preset-env', '@babel/preset-react'],
-												plugins: [
-													"@babel/plugin-transform-runtime",
-													"@babel/plugin-proposal-class-properties"
-												]
+					"loader": "babel-loader",
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+						plugins: [
+							"@babel/plugin-transform-runtime",
+							"@babel/plugin-proposal-class-properties"
+					    ]
                     }
                 }
             },
-            // {
-            //     "test": /\.css$/,
-            //     "use": [
-            //         "style-loader",
-            //         "css-loader"
-            //     ]
-            // },
+           
             {
                 "test": /\.html$/,
                 "use": [
